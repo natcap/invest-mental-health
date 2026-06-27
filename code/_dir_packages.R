413b76eb8bd55cd41d064601fe702ab054f1c1e5
Yingjie Li <yingjieli.edu@gmail.com>
2026-06-27 06:28:55 -0700

    to include overall SVI data columns

--- code/_dir_packages.R
+++ code/_dir_packages.R
@@ -9,3 +9,10 @@ library(dplyr)
 library(tidyr)
 library(stringr)
 library(ggplot2)
+
+
+library(tidycensus)
+# load your Census API key from an environment variable
+readRenviron("~/.Renviron")
+api_key <- Sys.getenv("MY_Census_API_KEY") 
+census_api_key(api_key, install = TRUE, overwrite = TRUE)
